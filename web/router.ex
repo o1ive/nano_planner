1c25b3735001a5e140203107d175a2a354625c94
o1ive <o1ive3220@gmail.com>
2018-11-11 16:31:54 +0900

    initial commit

---
+++ web/router.ex
@@ -0,0 +1,16 @@
+defmodule NanoPlanner.Router do
+  use NanoPlanner.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", NanoPlanner do
+    pipe_through :browser
+
+  end
+end
